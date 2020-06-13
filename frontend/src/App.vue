69e4dc93325a4f528125c120eb5da700fc636a7a
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-06-12 23:44:54 -0300

    Vuex + menuToggle added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="{'hide-menu': !isMenuVisible}">
     <Header title="Cod3r - Base de Conhecimento" :hideToggle="false" />
     <Menu />
     <Content />
@@ -8,6 +8,8 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 import Header from './components/templates/Header.vue';
 import Menu from './components/templates/Menu.vue';
 import Content from './components/templates/Content.vue';
@@ -18,6 +20,7 @@ export default {
   components: {
     Header, Menu, Content, Footer,
   },
+  computed: mapState(['isMenuVisible']),
 };
 </script>
 
@@ -42,5 +45,12 @@ export default {
       'header header'
       'menu content'
       'menu footer';
+  }
+
+  #app.hide-menu {
+    grid-template-areas:
+      'header header'
+      'content content'
+      'footer footer'
   }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,12 @@
 import 'font-awesome/css/font-awesome.css';
-
 import Vue from 'vue';
+
 import App from './App.vue';
+import store from './config/store';
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
+  store,
 }).$mount('#app');
