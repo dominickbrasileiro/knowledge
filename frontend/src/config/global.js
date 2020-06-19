3528fcb37f128fa3e95ae61c6ccc57f43eb7fb56
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-06-19 03:02:56 -0300

    showError updated

--- frontend/src/config/global.js
+++ frontend/src/config/global.js
@@ -1,15 +1,13 @@
+/* eslint-disable import/prefer-default-export */
 import Vue from 'vue';
 
-function showError(e) {
-  if (e && e.response && e.response.data) {
-    Vue.toasted.global.defaultError({ msg: e.response.data });
+export function showError(e) {
+  if (e && e.response && e.response.data && e.response.data.message) {
+    const msg = e.response.data.message || e.response.data;
+    Vue.toasted.global.defaultError({ msg });
   } else if (typeof e === 'string') {
     Vue.toasted.global.defaultError({ msg: e });
   } else {
     Vue.toasted.global.defaultError();
   }
 }
-
-export default {
-  showError,
-};
